88d5dbc91970c8b38358cd1b62577a2d9b981986
1755616537 <1755616537@qq.com>
2020-09-18 09:40:20 +0800

    no message

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,13 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    public function Return( $msg, $code = 0, $data='' ) {
+        return response()
+        ->json( [
+            'code'=>$code,
+            'msg' =>$msg,
+            'data'=>$data
+        ], 200 );
+    }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,20 +10,10 @@ use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller {
     public function index( Request $request ) {
-        // $name = $request->input( 'name' );
-        // return response()
-        // ->json( ['a'=>'2'] )
-        // ->header( '1', 'a' )
-        // ->cookie( 'a', '1' );
+        $name = $request->input( 'name' );
         Log::debug( 'An informational message.' );
         return $this->Return('成功')->cookie( 'a', '1' );
     }
-    protected function Return( $msg, $code = 0, $data='' ) {
-        return response()
-        ->json( [
-            'code'=>$code,
-            'msg' =>$msg,
-            'data'=>$data
-        ], 200 );
-    }
+
+    
 }
